f0313944d2a3104637f8fd6bd96b41c575e20be3
Constantin Vizir <vizirconstantin.89@gmail.com>
2024-10-08 00:19:52 +0300

    delete employee implemented

--- src/app/employee-table/employee-table.component.ts
+++ src/app/employee-table/employee-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../models/employee';
 import { EmployeeService } from '../employee.service';
 import { CommonModule, NgFor } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'employee-table',
@@ -14,13 +15,28 @@ export class EmployeeTableComponent implements OnInit {
 
   employees: Employee[] = [];
 
-  constructor(private employeeService: EmployeeService){}
+  constructor(private employeeService: EmployeeService, private router: Router){}
 
   ngOnInit(){
     this.employeeService.getEmployees().subscribe((data: Employee[]) => {
       this.employees = data;
       console.log(data);
     })
+  }
+
+  deleteEmployee(id: number): void {
+    this.employeeService.deleteEmployee(id).subscribe({
+      next: () => {
+        this.employees = this.employees.filter(e => e.id !== id);
+      },
+      error: (err) => {
+        console.log('Error deleting employee', err);
+      }
+    })
+  }
+
+  editEmployee(id: number): void {
+    this.router.navigate(['/edit', id]);
   }
 }
 
